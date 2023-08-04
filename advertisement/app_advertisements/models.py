f3c5302bc105ba89e0e6fd5343378b343165ed93
KiberOlga <o.kiber00@mail.ru>
2023-08-04 15:37:53 +0300

    Домашнее задание к 8 занятию

--- advertisement/app_advertisements/models.py
+++ advertisement/app_advertisements/models.py
@@ -1,5 +1,8 @@
 # -*- coding: cp1251 -*-
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
+from django.utils import timezone
 
 
 class Advertisement(models.Model):
@@ -9,6 +12,26 @@ class Advertisement(models.Model):
     auction = models.BooleanField("Торг", help_text="Отметьте, если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+
+    @admin.display(description='дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weight: bold; ">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y")
+
+    @admin.display(description='дата обновления')
+    def updated_date(self):
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: purple; font-weight: bold; ">Сегодня в {}</span>', updated_time
+            )
+        return self.updated_at.strftime("%d.%m.%Y")
 
     class Meta:
         db_table = 'advertisements'

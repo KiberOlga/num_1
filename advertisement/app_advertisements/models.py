0f82af4dbb1498c5512b5cf26fb655ea6e63978d
KiberOlga <o.kiber00@mail.ru>
2023-08-10 20:32:27 +0300

    9 занятие

--- advertisement/app_advertisements/models.py
+++ advertisement/app_advertisements/models.py
@@ -3,8 +3,9 @@ from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils import timezone
+from django.contrib.auth import get_user_model
 
-
+User = get_user_model()
 class Advertisement(models.Model):
     title = models.CharField("Заголовок", max_length=128)
     description = models.TextField("Описание")
@@ -12,6 +13,7 @@ class Advertisement(models.Model):
     auction = models.BooleanField("Торг", help_text="Отметьте, если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE)
 
 
 
--- advertisement/app_advertisements/views.py
+++ advertisement/app_advertisements/views.py
@@ -1,9 +1,12 @@
 # -*- coding: cp1251 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Advertisement
 
 def index(request):
-    return HttpResponse(request, 'index.html')
+    advertisements = Advertisement.objects.all()
+    context = {'advertisements': advertisements}
+    return render(request, 'index.html', context)
 
 
 def top_sellers(request):
